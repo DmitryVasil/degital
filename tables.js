c425ce751b8943822d380b5876c340c1759757bd
Dmitriy <vasilievdmitriy73@yandex.ru>
2023-05-26 14:28:52 +0400

    degital

---
+++ tables.js
@@ -0,0 +1,49 @@
+const tables = [
+  {
+    img: './img/table-1.jpg',
+    descr: 'стол 1',
+    date: '15.12.2021'
+  },
+  {
+    img: './img/table-4.jpg',
+    descr: 'стол 2',
+    date: '2017-01-26'
+  },
+  {
+    img: './img/table-1.jpg',
+    descr: 'стол 3',
+    date: '2017-01-26'
+  },
+  {
+    img: './img/table-4.jpg',
+    descr: 'стол 4',
+    date: '2017-01-26'
+  },
+  {
+    img: './img/table-1.jpg',
+    descr: 'стол 1',
+    date: '15.12.2021'
+  },
+  {
+    img: './img/table-4.jpg',
+    descr: 'стол 2',
+    date: '2017-01-26'
+  },
+  {
+    img: './img/table-1.jpg',
+    descr: 'стол 3',
+    date: '2017-01-26'
+  },
+  {
+    img: './img/table-1.jpg',
+    descr: 'стол 3',
+    date: '2017-01-26'
+  },
+  {
+    img: './img/table-1.jpg',
+    descr: 'стол 3',
+    date: '2017-01-26'
+  },
+]
+
+// export default tables
